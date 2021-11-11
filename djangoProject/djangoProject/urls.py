e8855d35c5be784c00693b5695a2952775b2be7d
Nitish Kumar <nitish@pushowl.com>
2021-11-11 13:24:00 +0530

    added:requirements
    also fixed some issues i find in my previous work

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("apiDrfDocker.urls"), name="apiDrfDocker"),
+    path('api/', include("apiDrfDocker.urls", namespace="apiDrfDocker")),
 ]
